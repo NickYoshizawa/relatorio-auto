dc53fc70d4d0381c4df73fa772ba646b5a0671fe
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-04-27 15:49:46 -0300

    Arrumando os parametros

--- components/DefaultFrame.py
+++ components/DefaultFrame.py
@@ -9,14 +9,22 @@ from config.theme import (
 )
 
 class DefaultFrame(ctk.CTkFrame):
-    def __init__(self, master=None, **kwargs):
-        super().__init__(
-            master=master,
-            width=FRAME_WIDTH,
-            height=FRAME_HEIGHT,
-            corner_radius=FRAME_CORNER_RADIUS,
-            border_width=FRAME_BORDER_WIDTH,
-            fg_color=FRAME_BG_COLOR,
-            border_color=FRAME_BORDER_COLOR,
-            **kwargs
-        )
+    
+    def __init__(self, 
+        master, 
+        width = FRAME_WIDTH, 
+        height = FRAME_HEIGHT, 
+        corner_radius = FRAME_CORNER_RADIUS, 
+        border_width = FRAME_BORDER_WIDTH, 
+        bg_color = "transparent", 
+        fg_color = FRAME_BG_COLOR, 
+        border_color = FRAME_BORDER_COLOR, 
+        background_corner_colors = None, 
+        overwrite_preferred_drawing_method = None, 
+        **kwargs
+    ):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+    
+
+    
+    
