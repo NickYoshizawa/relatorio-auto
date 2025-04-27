06879be289dcb7ac57c7031595be9a74c63969a3
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-04-27 18:48:00 -0300

    Adicionando campos

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,4 +1,5 @@
-from customtkinter import *
+import os
+import customtkinter as ctk
 import json
 import datetime
 
@@ -6,37 +7,55 @@ from utils.layout import *
 from utils.jsonUtils import update_json
 
 from components.DefaultButton import DefaultButton
-from components.DefaultEntry import DefaultEntry
 from components.LabelTextbox import LabelTextbox
 from components.LabelEntry import LabelEntry
+from components.FilesEntry import FilesEntry
+from components.DefaultFrame import DefaultFrame
+from components.DefaultRadioButton import DefaultRadioButton
 
-from config.theme import WINDOW_ICON
+from config.theme import WINDOW_ICON, EXCEL_FILETYPES
 
-class MainWindow(CTk):
+class MainWindow(ctk.CTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
+        self.file_type_option = ctk.StringVar(value="sheets") 
+        self.field_type_option = ctk.StringVar(value="row") 
+        
         if os.path.exists('data/token.json'):
             os.remove('data/token.json')
             
-        set_appearance_mode('dark-blue')
-        self.geometry('800x500')
-        self.minsize(600, 550)
+        ctk.set_appearance_mode('dark-blue')
+        self.geometry('800x550')
+        self.minsize(800, 550)
         self.iconbitmap(WINDOW_ICON)
         self.title('Integração GoogleSheets')
         
-        self.entrys_frame = CTkFrame(self, fg_color='transparent')
-        self.textboxs_frame = CTkFrame(self, fg_color='transparent')
+        self.file_radio_frame = DefaultFrame(self, border_width=0)
+        self.entrys_frame = DefaultFrame(self, border_width=0)
+        self.textboxs_frame = DefaultFrame(self, border_width=0)
+        self.field_type_radio_frame = DefaultFrame(self, border_width=0)
         
-        self.id_entry = LabelEntry(self.entrys_frame, label_text='Id da Planilha')
-        self.range_entry = LabelEntry(self.entrys_frame, label_text='Intervalo da Planilha')
-        self.date_entry = LabelEntry(self.entrys_frame, placeholder_text='ex: dd/mm/yyyy')
+        
+        self.sheets_radio_entry = DefaultRadioButton(self.file_radio_frame, text="Planilha Google Sheets", value="sheets", variable=self.file_type_option)
+        self.excel_radio_entry = DefaultRadioButton(self.file_radio_frame, text="Arquivo Excel", value="excel", variable=self.file_type_option)
+        self.after(100, self.radio_callback)
+        self.excel_radio_entry.bind("<Button-1>", self.radio_callback)
+        self.sheets_radio_entry.bind("<Button-1>", self.radio_callback)
+        
+        self.excel_entry = FilesEntry(self.entrys_frame, filetypes=EXCEL_FILETYPES, height=63, wrap_lenth=400)
+        self.sheets_entry = LabelEntry(self.entrys_frame, label_text='Link da planilha (.CSV):', border_width=0)
+        
+        self.field_type_entry = LabelEntry(self.entrys_frame, label_text="Campo:",border_width=0)
+        
+        self.row_radio_entry = DefaultRadioButton(self.field_type_radio_frame, text="Buscar por linha", value="row", variable=self.field_type_option)
+        self.column_radio_entry = DefaultRadioButton(self.field_type_radio_frame, text="Buscar por coluna", value="column", variable=self.field_type_option)
         
         self.input_textbox = LabelTextbox(self.textboxs_frame, text='Input:', fg_color='transparent')
-        self.output_textbox = LabelTextbox(self.textboxs_frame, text='Output:', fg_color='transparent')
+        self.output_textbox = LabelTextbox(self.textboxs_frame, text='Output:', fg_color='transparent', state='disabled')
         
-        self.submit_button = DefaultButton(self, text='Submit', command=self.__submit)
+        self.submit_button = DefaultButton(self, text='Submit', command=self.submit)
         
-        self.date_entry.insert(0, datetime.date.today().strftime('%d/%m/%Y'))
         
         try:
             with open('data/msg.txt', 'r', encoding="utf-8") as file:
@@ -46,28 +65,43 @@ class MainWindow(CTk):
                 json_data = json.loads(file.read())
                 
                 if json_data[0]['id'] != '' and json_data[0]['range'] != '':
-                    self.id_entry.insert(0, json_data[0]['id'])
+                    self.sheets_entry.insert(0, json_data[0]['id'])
                     self.range_entry.insert(0, json_data[0]['range'])
                 
         except:
             pass
     
-        self.entrys_frame.pack(fill=X)
-        self.textboxs_frame.pack(fill=BOTH, expand=True, pady=(20, 0))
+        self.file_radio_frame.pack(fill='x', pady=(20, 10))
+        self.entrys_frame.pack(fill='x')
+        self.field_type_radio_frame.pack(fill='x', pady=10)
+        self.textboxs_frame.pack(fill='both', expand=True, pady=(10, 0))
         
         
-        self.id_entry.pack(expand=True, fill=X, padx=100)
-        self.range_entry.pack(expand=True, fill=X, padx=100)
-        self.date_entry.pack(expand=True, fill=X, padx=100)
+        self.sheets_radio_entry.pack(anchor="w", side="left", padx=(100, 20))
+        self.excel_radio_entry.pack(anchor="w")
         
-        self.input_textbox.pack(expand=True, fill=BOTH, side='left', padx=20)
-        self.output_textbox.pack(expand=True, fill=BOTH, side='right', padx=(0,20))
+        self.field_type_entry.pack(expand=True, fill='x', padx=100)
+        
+        self.row_radio_entry.pack(anchor="w", side="left", padx=(100, 20))
+        self.column_radio_entry.pack(anchor="w")
+        
+        self.input_textbox.pack(expand=True, fill='both', side='left', padx=20)
+        self.output_textbox.pack(expand=True, fill='both', side='right', padx=(0,20))
         
         self.submit_button.pack(pady=30)
+    
+    def radio_callback(self, event=None):
+        if self.file_type_option.get() == "sheets":
+            self.excel_entry.pack_forget()
+            self.sheets_entry.pack(expand=True, fill='x', padx=100 ,before=self.field_type_entry)
+        else:
+            self.sheets_entry.pack_forget()
+            self.excel_entry.pack(expand=True, fill='x', padx=100, before=self.field_type_entry)
+            
         
-    def __submit(self):
+    def submit(self):
         
-        self.output_textbox.delete("1.0", "end")
+        """self.output_textbox.delete("1.0", "end")
         
         open('data/msg.txt', 'w').close()
         open('data/inputs.json', 'w').close()
@@ -85,9 +119,14 @@ class MainWindow(CTk):
         with open('data/msg.txt', 'a', encoding="utf-8") as file:
             file.write(self.msg)
             
-        update_json('data/inputs.json', data, 2, False)
+        update_json('data/inputs.json', data, 2, False)"""
         
-        self.output_textbox.insert(1.0, integracao(self.msg, self.id, self.range, self.date))
+        # self.output_textbox.insert(1.0, integracao(self.msg, self.id, self.range, self.date))
+        
+        
+        self.output_textbox.configure(state="normal")
+        self.output_textbox.insert(1.0, "Apenas um teste")
+        self.output_textbox.configure(state="disabled")
         
     def run(self):
         self.mainloop()
