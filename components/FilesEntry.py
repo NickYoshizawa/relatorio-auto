5b4507cfe722bd242d008d34629357eeddd76dec
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-04-27 18:47:28 -0300

    Criando novo widget

--- components/FilesEntry.py
+++ components/FilesEntry.py
@@ -1,35 +1,58 @@
-from typing import Any, Tuple
-import customtkinter as ctk
+from typing import Any, Iterable
 from tkinter import filedialog
 
+from components.DefaultFrame import DefaultFrame
+from components.DefaultLabel import DefaultLabel
+from components.DefaultButton import DefaultButton
 
-class FileEntry(ctk.CTkFrame):
-    def __init__(self, master: Any, wrap_lenth: int = 690, width: int = 200, height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = 1, bg_color: str | Tuple[str] = "transparent", fg_color: str | Tuple[str] | None = None, border_color: str | Tuple[str] | None = 'white', background_corner_colors: Tuple[str | Tuple[str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
-        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+from config.theme import (
+    BUTTON_WIDTH,
+    BUTTON_HEIGHT
+)
 
+class FilesEntry(DefaultFrame):
+    def __init__(self, 
+        master: Any, 
+        wrap_lenth: int = 690,
+        height: int = None,
+        button_height: int = BUTTON_HEIGHT,
+        button_width: int = BUTTON_WIDTH,
+        label_text: str = "Escolha um arquivo",
+        filetypes: Iterable[tuple[str, str | list[str] | tuple[str, ...]]] | None = [("Todos os arquivos", "*.*")],
+        **kwargs
+    ):
         
+        
+        super().__init__(master, **kwargs)
+        
+        if height:
+            self.pack_propagate(False)
+            self.configure(height=height)
+        
+        self.label_text = label_text
+        self.filetypes = filetypes
     
-        self.label = CTkLabel(self, text="Selecione um Arquivo Excel", wraplength=wrap_lenth)
-        self.button= CTkButton(self, text='Browse', command=self.__browsefile, height=40, width=100)
+        self.label = DefaultLabel(self, text=self.label_text, wraplength=wrap_lenth)
+        self.button= DefaultButton(self, text='Browse', command=self.__browsefile, height=button_height, width=button_width)
         
-        self.label.pack(side=ctk.LEFT, padx=10, pady=10, fill=ctk.BOTH)
-        self.button.pack(side=ctk.RIGHT, padx=10, pady=10)
+        self.label.pack(side="left", padx=10, pady=10, fill="both")
+        self.button.pack(side="right", padx=10, pady=10)
         
         self.filepath = []
     
     def __browsefile(self):
-        self.filepath = filedialog.askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
+        self.filepath = filedialog.askopenfilename(filetypes=self.filetypes)
         if self.filepath:
             self.label.configure(text=self.filepath)
         else:
-            self.label.configure(text='Selecione um Arquivo Excel')
+            self.label.configure(text=self.label_text)
         
     def insert(self, string):
         self.label.configure(text=string)
     
     def get(self):
         file = self.label.cget("text")
-        if file == '' or file == "Selecione um Arquivo Excel":
+        if file == '' or file == self.label_text:
             return ''
         else:
             return file
